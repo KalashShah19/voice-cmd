bef8b107de2af03e751478e5297f0f59384e303e
KalashShah19 <kalashgamer19@gmail.com>
2024-03-10 23:51:47 +0530

    Day 4
    artificial intelligence ui
    show AI page in alert box
    screen recording

--- app.py
+++ app.py
@@ -1,49 +1,33 @@
 from flask import Flask, render_template, request, jsonify
-import speech_recognition as sr
-import tempfile
-import subprocess
 import os
+import time
+import speech_recognition as sr
 
 app = Flask(__name__)
+app = Flask(__name__, static_folder='templates/static')
+
+# Global variable to store recorded audio
+recorded_audio_path = None
 
 @app.route('/')
 def index():
-    return render_template('test.html')
+    return render_template('voice.html')
 
-@app.route('/record', methods=['POST'])
-def record_audio():
-    temp_audio_file = tempfile.NamedTemporaryFile(suffix='.wav', delete=False)
-    audio_file_path = temp_audio_file.name
-    temp_audio_file.close()
-    print(audio_file_path)
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
-    duration = request.form.get('duration', 5)  # Default recording duration is 5 seconds
+@app.route('/features')
+def features():
+    return render_template('features.html')
 
-    # Use sox to record audio
-    cmd = ['sox', '-d', '-t', 'wav', audio_file_path, 'trim', '0', str(duration)]
-    subprocess.run(cmd)
+@app.route('/ai')
+def ai():
+    return render_template('ai.html')
 
-    return jsonify({'audio_file_path': audio_file_path})
-
-@app.route('/recognize', methods=['POST'])
-def recognize_speech():
-    if 'audio_file_path' not in request.form:
-        return jsonify({'error': 'No audio file provided'}), 400
-
-    audio_file_path = request.form['audio_file_path']
-
-    recognizer = sr.Recognizer()
-
-    with sr.AudioFile(audio_file_path) as source:
-        audio_data = recognizer.record(source)
-
-    try:
-        text = recognizer.recognize_google(audio_data)
-        return jsonify({'transcription': text}), 200
-    except sr.UnknownValueError:
-        return jsonify({'error': 'Could not understand audio'}), 400
-    except sr.RequestError as e:
-        return jsonify({'error': f'Speech recognition service error: {e}'}), 500
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
